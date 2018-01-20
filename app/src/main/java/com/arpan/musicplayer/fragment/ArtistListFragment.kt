3f41c23b91378bfa3c00c278e24a8e5b313a7578
Arpan Srivastava <arpansri98@gmail.com>
2018-01-21 01:38:11 +0530

    Fixed ArtistListFragment, and added code to download the ArtistImages from Last.fm

--- app/src/main/java/com/arpan/musicplayer/fragment/ArtistListFragment.kt
+++ app/src/main/java/com/arpan/musicplayer/fragment/ArtistListFragment.kt
@@ -2,16 +2,24 @@ package com.arpan.musicplayer.fragment
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.transition.*
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.arpan.musicplayer.GlideApp
 import com.arpan.musicplayer.R
 import com.arpan.musicplayer.activity.MainActivity
@@ -20,6 +28,10 @@ import com.arpan.musicplayer.model.Artist
 import kotlinx.android.synthetic.main.fragment_artist_list.*
 import com.arpan.musicplayer.rest.AutoFitGridLayoutManager
 import kotlinx.android.synthetic.main.artist_list_item.*
+import java.io.IOException
+import java.io.InputStream
+import khttp.get
+import org.json.JSONObject
 
 // Created on 19/12/2017
 
@@ -85,6 +97,7 @@ class ArtistListFragment: Fragment(), ArtistListAdapter.HandleCallbackFromAdapte
     }
 
     private var mArtistsList: ArrayList<Artist> = ArrayList()
+    private var mTopArtistsList: ArrayList<Artist> = ArrayList(9)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_artist_list, container, false)
@@ -97,14 +110,22 @@ class ArtistListFragment: Fragment(), ArtistListAdapter.HandleCallbackFromAdapte
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {}
 
+        artistFragmentHolder.isSmoothScrollingEnabled = true
+
 //        setUpTransitions()
+        try {
+            val inputStream: InputStream = (activity as MainActivity).assets.open("img/artist_header.jpg")
+            val drawable: Drawable = Drawable.createFromStream(inputStream, null)
+            artistFragmentHeaderIV.setImageDrawable(drawable)
+            val inputStream2: InputStream = (activity as MainActivity).assets.open("img/artist_footer.jpeg")
+            val drawable2: Drawable = Drawable.createFromStream(inputStream2, null)
+            artistFragmentFooterIV.setImageDrawable(drawable2)
+        } catch (exception: IOException) {
+//            Toast.makeText(context, "Not Found.", Toast.LENGTH_SHORT).show()
+            Log.d("Error", "Exception: " + exception.stackTrace)
+        }
 
-        GlideApp
-                .with(context)
-                .load(Uri.parse("file:///assets/img/artist_header.jpg"))
-                .placeholder(R.drawable.ic_compact_disc)
-                .into(artistFragmentHeaderIV)
-
+        artistFragmentHolder.smoothScrollTo(0, 0)
 
         val loadArtistsTask = LoadArtistsTask()
         loadArtistsTask.execute()
@@ -136,13 +157,29 @@ class ArtistListFragment: Fragment(), ArtistListAdapter.HandleCallbackFromAdapte
     }
 
     private fun setUpRecyclerView() {
-        val adapter = ArtistListAdapter(context, mArtistsList)
+        val adapter = ArtistListAdapter(context, mTopArtistsList)
         adapter.setAdapterCallback(this@ArtistListFragment)
         artistRecyclerView.adapter = adapter
 
-        val layoutManager = AutoFitGridLayoutManager(context, 500)
+        val layoutManager = GridLayoutManager(context, 3, LinearLayout.VERTICAL, false)
         artistRecyclerView.layoutManager = layoutManager
+        artistRecyclerView.isClickable = false
 
+        artistRecyclerView.addOnItemTouchListener(object : RecyclerView.OnItemTouchListener {
+            override fun onTouchEvent(rv: RecyclerView?, e: MotionEvent?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onInterceptTouchEvent(rv: RecyclerView?, e: MotionEvent?): Boolean {
+                artistFragmentHolder.onInterceptTouchEvent(e)
+                return false
+            }
+
+            override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 
     private fun loadArtists() {
@@ -165,11 +202,15 @@ class ArtistListFragment: Fragment(), ArtistListAdapter.HandleCallbackFromAdapte
                     mArtistsList.add(artist)
                 } while (cursor.moveToNext())
 
-                //Remove items if more than 9
-                if (mArtistsList.size > 9) {
-                    for (i in 9..mArtistsList.size) {
-                        mArtistsList.removeAt(i)
-                    }
+                //Add items top Artists List
+
+                for (i in 0..8) {
+                    val artist = mArtistsList[i]
+                    val response = get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + artist.artistName +  "&api_key=8e71dd998e907fa081fdf059691e10c9&format=json")
+                    val jsonArray = response.jsonArray
+                    //TODO("CONTINUE HERE")
+                    val imageURL = imageObject.
+                    mTopArtistsList.add(mArtistsList[i])
                 }
 
                 cursor.close()
